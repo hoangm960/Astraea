996463118e3bbd5cf467aeda4d53a49068ccf36f
nhatminh2901 <63330514+nhatminh2901@users.noreply.github.com>
2020-10-04 13:45:34 +0700

    Upload Assistant

---
+++ Assistant/Assistant.py
@@ -0,0 +1,114 @@
+from tkinter import *
+from tkinter import messagebox
+import time
+from tqdm import tqdm
+import sys
+import codecs
+import webbrowser
+
+chrome_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+
+#----------------------------------------------------------------
+#SignBox
+def sign():
+    SignRoot = Tk()
+    SignRoot.resizable(0,0)
+    SignRoot.title('Account box')
+    SignRoot.geometry('750x500+250+100')
+    SignInBox = Canvas(SignRoot, width = 750, height = 500, bg = 'lightblue')
+    Textbox = Label(SignInBox, text = 'SIGN UP YOUR NEW USER', bg = 'lightblue', fg = 'blue', font = ('Arial Bold',20))
+    Textbox.place(relx = 0.5, rely = 0.1, anchor = 'n')
+    Name = Label(SignInBox, text = 'Your name *:', fg = 'black', bg = 'lightblue', font = 50)
+    Name.place(relx = 0.25, rely = 0.25)
+    Nameentry = Entry(SignInBox, font = 40)
+    Nameentry.place(relx = 0.5, rely=0.3, relwidth = 0.45, relheight = 0.08, anchor = 'n')
+    Name = Label(SignInBox, text = 'Your facebook link: (skip to default: facebook.com)', fg = 'black', bg = 'lightblue', font = 50)
+    Name.place(relx = 0.25, rely = 0.4)
+    FBentry = Entry(SignInBox, font = 40)
+    FBentry.place(relx = 0.5, rely=0.45, relwidth = 0.45, relheight = 0.08, anchor = 'n')
+    Name = Label(SignInBox, text = 'Your email link: (skip to default: gmail.com)', fg = 'black', bg = 'lightblue', font = 50)
+    Name.place(relx = 0.25, rely = 0.55)
+    EMentry = Entry(SignInBox, font = 40)
+    EMentry.place(relx = 0.5, rely=0.6, relwidth = 0.45, relheight = 0.08, anchor = 'n')
+    Webname = StringVar(SignInBox)
+    Webname.set('Choose your Webbrowser:')
+    MenuOption = OptionMenu(SignInBox, Webname,"Google Chrome","Internet Explorer")
+    MenuOption.place(relx = 0.5, rely = 0.7, relwidth = 0.25, relheight = 0.07, anchor = 'n')
+    SignInBox.pack()
+    def saves():
+        again = True
+        with open(r'data\User.txt','w') as f:
+            newname = Nameentry.get()
+            error = Label(SignInBox)
+            if newname == '':
+                error = Label(SignInBox, text = "You should sign up your Name", bg = 'lightblue', fg = 'red', font = 30)
+                error.place(relx = 0.4, rely = 0.25, relwidth = 0.45)  
+            elif Webname.get() == 'Choose your Webbrowser:':
+                error = Label(SignInBox, text = "You have to choose Webbrowser", bg = 'lightblue', fg = 'red', font = 30)
+                error.place(relx = 0.4, rely = 0.25, relwidth = 0.45)
+            else:
+                for i in newname:
+                    if i not in 'qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM1234567890 ':
+                        error = Label(SignInBox, text = "Your name musn't contain any special characters", bg = 'lightblue', fg = 'red', font = 30)
+                        error.place(relx = 0.4, rely = 0.25)
+                        again = False
+                if again == True:
+                    f.write(newname)
+                    newlink = '\n' + FBentry.get()
+                    if newlink == '\n':
+                        newlink+='Facebook.com'
+                    f.write(newlink)
+                    newGmail = '\n' + EMentry.get()
+                    if newGmail == '\n':
+                        newGmail+='Gmail.com'
+                    f.write(newGmail)
+                    newWeb = '\n' + str(Webname.get())
+                    f.write(newWeb)
+                    SignInBox.destroy()
+                    complete = Label(SignRoot, text = 'You have been completed.', bg = 'white', fg = 'blue', font = ('Arial Bold',20))
+                    complete.place(relx = 0.5, rely = 0.4, anchor = 'n')
+                    f.close()
+    SaveButton = Button(SignInBox, text = 'SIGN UP', activebackground = 'lightgreen', command = lambda: saves(), relief = FLAT)
+    SaveButton.place(relx = 0.5, rely = 0.8, relwidth = 0.15, relheight = 0.08, anchor = 'n')
+    SignRoot.wait_window()
+        
+# --------------------------------------
+
+
+#------Root
+with open('data/User.txt','r') as f:
+    check = f.readline()
+    User = ['','','','']
+    if str(check) == '':
+        sign()
+    f.close()
+with open('data/User.txt','r') as f:
+    User[0] = (f.readline().replace('\n',''))
+    User[1] = (f.readline().replace('\n',''))
+    User[2] = (f.readline().replace('\n',''))
+    User[3] = (f.readline().replace('\n',''))
+    if User[0] == '': 
+        messagebox.showinfo('Notification','You have to complete this form.')
+        exit()
+    if User[3] == 'Google Chrome':
+        web_path = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s'
+    elif User[3] == 'Internet Explorer':
+        web_path = 'C:/Program Files/internet explorer/iexplore.exe %s'
+    f.close()
+
+root = Tk()
+root.title('Assistant')
+root.resizable(0,0)
+root.geometry('300x735+1055+0')
+root.attributes('-toolwindow',1)
+BackgroundMain = Canvas(root, width = 300, height = 750, bg = 'lightblue')
+BackgroundMain.pack()
+
+#-----Function
+YTphoto = PhotoImage(file = 'icons\youtubeButton.png')
+photoYT = YTphoto.subsample(3,3)
+FB = Button(BackgroundMain, image = photoYT, relief = FLAT, command = lambda: webbrowser.get(web_path).open('youtube.com'))
+FB.place(relx = 0.2, rely =0.3)
+
+root.mainloop()
+root.wait_window

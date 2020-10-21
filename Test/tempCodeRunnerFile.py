d11b092d91163aaf308c793d725585a7e70a1592
nhatminh2901 <hoangm960@gmail.com>
2020-10-21 07:49:11 +0700

    clean up

--- Test/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-SignRoot.resizable(0,0)
-    SignRoot.wm_attributes("-topmost",1)
-    SignRoot.title('Cửa sổ đăng nhập - Pylearn')
-    SignRoot.iconbitmap('icons/logo.ico')
-    SignRoot.geometry('750x500+250+100')
-    

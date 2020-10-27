3a262170055bc33e8ba2c75c7e280213ae0401fe
nhatminh2901 <hoangm960@gmail.com>
2020-10-27 17:49:46 +0700

    fukin done with encription

--- encription/key.py
+++
@@ -1,9 +0,0 @@
-from cryptography.fernet import Fernet
-
-
-def main():
-    key = Fernet.generate_key()
-
-    file = open('encription/key.key', 'wb')  # Open the file as wb to write bytes
-    file.write(key)  # The key is type bytes still
-    file.close()

09fd5103be2f534e412756b9e016ebd7abb07e14
SharkTien <72133411+SharkTien@users.noreply.github.com>
2020-11-08 15:59:24 +0700

    Update frame_edit.py

--- frame_edit.py
+++ frame_edit.py
@@ -1,4 +1,5 @@
 from tkinter import *
+from tkinter import ttk
 import Login 
 from gui import GradientFrame
 from PIL import Image, ImageTk
@@ -39,6 +40,28 @@ def EditWindow():
     #     ConfigCanvas = self.MainCanvas(self, bg = '#6292bf', width = 800, height = 600)
     #     ConfigCanvas.configure(scrollregion= ConfigCanvas.bbox("all"))
     #     ConfigCanvas.create_frames(self.ContentNum)
+        class ScrollableFrame(ttk.Frame):
+            def __init__(self, container, *args, **kwargs):
+                super().__init__(container, *args, **kwargs)
+                self.frame = tk.Frame(self)
+                scrollbar = ttk.Scrollbar(self, orient="vertical", command=self.canvas.yview)
+                self.scrollable_frame = ttk.Frame(self.canvas)
+                
+
+                self.canvas.bind_all("<Configure>", self.onFrameConfigure)
+                
+                self.canvas.bind_all("<MouseWheel>", lambda e: self.canvas.yview_scroll(int(-1*(e.delta/120)), "units"))
+
+                self.canvas.create_window((0, 0), window=self.scrollable_frame, anchor="nw")
+
+                self.canvas.configure(yscrollcommand=scrollbar.set)
+
+                self.canvas.pack(side="left", fill="both", expand=True)
+                scrollbar.pack(side="right", fill="y")
+            
+            def onFrameConfigure(self, event):
+                self.canvas.configure(scrollregion = self.canvas.bbox("all"))
+
         def create_config_canvas(cannvas):
             frameLesson = Frame(cannvas)
             frameLesson.place(relx = 0, rely = 0, relwidth = 1, relheight = 0.1)

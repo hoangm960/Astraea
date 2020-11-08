43891494ee82c6d6609cbe26c9449b58f2b7f48a
nhatminh2901 <hoangm960@gmail.com>
2020-11-08 20:44:30 +0700

    done most front end

--- new gui/main.py
+++ new gui/main.py
@@ -1,36 +1,25 @@
 import sys
-from PyQt5.QtCore import Qt
+
+from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow
 
-from ui_main import Ui_MainWindow
 from ui_functions import *
+from ui_main import Ui_MainWindow
+
 
 class MainWindow(QMainWindow):
     def __init__(self):
-        QMainWindow.__init__(self)
+        QMainWindow.__init__(self, None, QtCore.Qt.WindowStaysOnTopHint)
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
 
-        def moveWindow(event):
-            if UIFunctions.returnStatus() == True:
-                UIFunctions.maximize_restore(self)
-            if event.buttons() == Qt.LeftButton:
-                self.move(self.pos() + event.globalPos() - self.dragPos)
-                self.dragPos = event.globalPos()
-                event.accept()
-
-        self.ui.title_bar.mouseMoveEvent = moveWindow
-
         UIFunctions.uiDefinitions(self)
-
-
-    def mousePressEvent(self, event):
-        self.dragPos = event.globalPos()
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     window = MainWindow()
+    window.move(1070, 0)
     window.show()
     sys.exit(app.exec_())
 

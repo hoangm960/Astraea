dcb1a48fa1a0956c7ac13c86f61733ca4d0d3d23
nhatminh2901 <hoangm960@gmail.com>
2020-11-12 14:25:09 +0700

    gui stuff

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import sys
 import win32con
 import win32gui
 from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import (QApplication, QDialogButtonBox,
                              QGraphicsDropShadowEffect, QMainWindow, QMessageBox,
@@ -13,6 +14,7 @@ from PyQt5.QtWidgets import (QApplication, QDialogButtonBox,
 from main import *
 from ui_main import Ui_MainWindow
 import pickle
+from edit_form import Ui_EditWindow
 
 
 class MainWindow(QMainWindow):
@@ -89,6 +91,7 @@ class UIFunctions(MainWindow):
         self.ui.main_btn.setText('Sửa đổi')
         self.ui.main_btn.setStyleSheet('''QPushButton {background-color: rgb(59, 143, 14);}
         QPushButton:hover {background-color: rgba(59, 143, 14, 150);}''')
+        self.ui.main_btn.clicked.connect(lambda: open_edit_form())
 
         self.ui.assignment_details.setReadOnly(False)
         self.ui.confirmButton = QDialogButtonBox(self.ui.frame_content_hint)
@@ -116,7 +119,7 @@ class UIFunctions(MainWindow):
             msg.setWindowTitle("Thành công sửa đổi bài tập")
             msg.setText("Chi tiết câu đã được chỉnh sửa")
             with open(filename) as f:
-                msg.setDetailedText(f'{save_text.details[self.ui.list_assignments.currentRow()]}\n ---> {self.ui.assignment_details.toPlainText()}')
+                msg.setDetailedText(f'Chi tiết gốc:\n{save_text.details[self.ui.list_assignments.currentRow()]}\n\nChi tiết sau khi sửa đổi:\n{self.ui.assignment_details.toPlainText()}')
             msg.setIcon(QMessageBox.Information)
             msg.setStandardButtons(QMessageBox.Ok|QMessageBox.Cancel)
             msg.buttonClicked.connect(popup_button)
@@ -124,6 +127,79 @@ class UIFunctions(MainWindow):
 
         def popup_button(i):
             save_text.changed = False if i.text().lower() == "cancel" else True
+
+        class UIFunctions(MainWindow):
+            GLOBAL_STATE = False
+
+            @classmethod
+            def maximize_restore(cls, self):
+                status = cls.GLOBAL_STATE
+
+                if status == False:
+                    self.showMaximized()
+
+                    cls.GLOBAL_STATE = True
+                    
+                    self.ui.bg_layout.setContentsMargins(0, 0, 0, 0)
+                    self.ui.bg_frame.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0.341, x2:1, y2:0.897, stop:0 rgba(97, 152, 255, 255), stop:0.514124 rgba(186, 38, 175, 255), stop:1 rgba(255, 0, 0, 255)); border-radius: 0px;")
+                    self.ui.btn_maximize.setToolTip("Restore")
+                else:
+                    cls.GLOBAL_STATE = False
+                    self.showNormal()
+                    self.resize(self.width() + 1, self.height() + 1)
+                    self.ui.bg_layout.setContentsMargins(10, 10, 10, 10)
+                    self.ui.bg_frame.setStyleSheet("background-color: qlineargradient(spread:pad, x1:0, y1:0.341, x2:1, y2:0.897, stop:0 rgba(97, 152, 255, 255), stop:0.514124 rgba(186, 38, 175, 255), stop:1 rgba(255, 0, 0, 255)); border-radius: 20px;")
+                    self.ui.btn_maximize.setToolTip("Maximize")
+
+            @classmethod
+            def uiDefinitions(cls, self):
+                self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+                self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+                self.shadow = QGraphicsDropShadowEffect(self)
+                self.shadow.setBlurRadius(20)
+                self.shadow.setXOffset(0)
+                self.shadow.setYOffset(0)
+                self.shadow.setColor(QColor(0, 0, 0, 100))
+                self.ui.bg_frame.setGraphicsEffect(self.shadow)
+
+                self.ui.btn_maximize.clicked.connect(lambda: UIFunctions.maximize_restore(self))
+                self.ui.btn_minimize.clicked.connect(lambda: self.showMinimized())
+                self.ui.btn_quit.clicked.connect(lambda: self.close())
+
+                self.sizegrip = QSizeGrip(self.ui.frame_grip)
+                self.sizegrip.setStyleSheet("QSizeGrip { width: 20px; height: 20px; margin: 5px; border-radius: 10px; } QSizeGrip:hover { background-color: rgb(201, 21, 8) }")
+                self.sizegrip.setToolTip("Resize Window")
+
+            @classmethod
+            def returnStatus(cls):
+                return cls.GLOBAL_STATE
+
+        class EditWindow(QMainWindow):
+            def __init__(self):
+                QMainWindow.__init__(self, None, QtCore.Qt.WindowStaysOnTopHint)
+                self.ui = Ui_EditWindow()
+                self.ui.setupUi(self)
+
+                def moveWindow(event):
+                    if UIFunctions.returnStatus() == True:
+                        UIFunctions.maximize_restore(self)
+                    if event.buttons() == Qt.LeftButton:
+                        self.move(self.pos() + event.globalPos() - self.dragPos)
+                        self.dragPos = event.globalPos()
+                        event.accept()
+
+                self.ui.title_bar.mouseMoveEvent = moveWindow
+
+                UIFunctions.uiDefinitions(self)
+
+
+            def mousePressEvent(self, event):
+                self.dragPos = event.globalPos()
+
+        def open_edit_form():
+            window = EditWindow()
+            window.show()
 
     @classmethod
     def student_gui_config(cls, self):

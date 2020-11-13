6d062a6f00271f8459686cf44feaba5e0e8835c5
nhatminh2901 <hoangm960@gmail.com>
2020-11-13 17:25:08 +0700

    CLEANNNEEERRRRR

--- main.py
+++ main.py
@@ -8,9 +8,16 @@ import win32gui
 from PyQt5 import QtCore
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QColor
-from PyQt5.QtWidgets import (QApplication, QDialogButtonBox,
-                             QGraphicsDropShadowEffect, QMainWindow,
-                             QMessageBox, QPushButton, QSizeGrip, QWidget)
+from PyQt5.QtWidgets import (
+    QApplication,
+    QDialogButtonBox,
+    QGraphicsDropShadowEffect,
+    QMainWindow,
+    QMessageBox,
+    QPushButton,
+    QSizeGrip,
+    QWidget,
+)
 from win32api import GetSystemMetrics
 
 from edit_main import EditWindow
@@ -21,13 +28,12 @@ from PyQt5 import uic
 class MainWindow(QMainWindow):
     def __init__(self, role):
         QMainWindow.__init__(self, None, QtCore.Qt.WindowStaysOnTopHint)
-        uic.loadUi('ui_main.ui', self)
+        uic.loadUi("ui_main.ui", self)
         self.role = role
         UIFunctions.uiDefinitions(self)
 
 
 class UIFunctions(MainWindow):
-
     @classmethod
     def uiDefinitions(cls, self):
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
@@ -46,16 +52,17 @@ class UIFunctions(MainWindow):
         # UIFunctions.open_vscode()
 
         UIFunctions.load_assignments(self, "assignments.txt")
-        self.list_assignments.itemActivated.connect(lambda: UIFunctions.load_details(self, 'assignment_details.list'))
+        self.list_assignments.itemActivated.connect(
+            lambda: UIFunctions.load_details(self, "assignment_details.list")
+        )
 
         UIFunctions.define_role(self)
-        
 
     @classmethod
     def open_vscode(cls):
         file = os.path.expandvars("%LOCALAPPDATA%/Programs/Microsoft VS Code/Code.exe")
         subprocess.call(file)
-        cls.pg = win32gui.FindWindow(None, 'Visual Studio Code')
+        cls.pg = win32gui.FindWindow(None, "Visual Studio Code")
         x0, y0, x1, y1 = win32gui.GetWindowRect(cls.pg)
         w = x1 - x0
         h = y1 - y0
@@ -64,7 +71,7 @@ class UIFunctions(MainWindow):
     @classmethod
     def close_pg(cls, self):
         try:
-            win32gui.PostMessage(cls.pg, win32con.WM_CLOSE,0,0)
+            win32gui.PostMessage(cls.pg, win32con.WM_CLOSE, 0, 0)
         except:
             pass
 
@@ -76,25 +83,29 @@ class UIFunctions(MainWindow):
             lines = f.readlines()
             for line in lines:
                 self.list_assignments.addItem(line.rstrip("\n"))
-    
+
     @classmethod
     def load_details(cls, self, filename):
-        with open(filename, 'rb') as f:
+        with open(filename, "rb") as f:
             details = pickle.load(f)
-            self.assignment_details.setText(details[self.list_assignments.currentRow()].rstrip('\n'))
+            self.assignment_details.setText(
+                details[self.list_assignments.currentRow()].rstrip("\n")
+            )
 
     @classmethod
     def define_role(cls, self):
-        if self.role.lower() == 'teacher':
+        if self.role.lower() == "teacher":
             cls.teacher_gui_config(self)
-        if self.role.lower() == 'student':
+        if self.role.lower() == "student":
             cls.student_gui_config(self)
-    
+
     @classmethod
     def teacher_gui_config(cls, self):
-        self.main_btn.setText('Sửa đổi')
-        self.main_btn.setStyleSheet('''QPushButton {background-color: rgb(59, 143, 14);}
-        QPushButton:hover {background-color: rgba(59, 143, 14, 150);}''')
+        self.main_btn.setText("Sửa đổi")
+        self.main_btn.setStyleSheet(
+            """QPushButton {background-color: rgb(59, 143, 14);}
+        QPushButton:hover {background-color: rgba(59, 143, 14, 150);}"""
+        )
         self.main_btn.clicked.connect(lambda: open_edit_form())
 
         self.assignment_details.setReadOnly(False)
@@ -102,36 +113,43 @@ class UIFunctions(MainWindow):
         self.confirmButton.setStandardButtons(QDialogButtonBox.Ok)
         self.confirmButton.setObjectName("confirmButton")
         self.verticalLayout_4.addWidget(self.confirmButton)
-        self.confirmButton.accepted.connect(lambda: save_text('assignment_details.list'))
-        
+        self.confirmButton.accepted.connect(
+            lambda: save_text("assignment_details.list")
+        )
+
         def save_text(filename):
             save_text.changed = True
-            with open(filename, 'rb') as f:
+            with open(filename, "rb") as f:
                 save_text.details = pickle.load(f)
-            if save_text.details[self.list_assignments.currentRow()] != self.assignment_details.toPlainText():
+            if (
+                save_text.details[self.list_assignments.currentRow()]
+                != self.assignment_details.toPlainText()
+            ):
                 show_confirm_mess(filename)
-            save_text.details[self.list_assignments.currentRow()] = self.assignment_details.toPlainText()
+            save_text.details[
+                self.list_assignments.currentRow()
+            ] = self.assignment_details.toPlainText()
             if save_text.changed:
-                with open(filename, 'wb') as f:    
+                with open(filename, "wb") as f:
                     pickle.dump(save_text.details, f)
             else:
                 cls.load_details(self, filename)
-
 
         def show_confirm_mess(filename):
             msg = QMessageBox()
             msg.setWindowTitle("Thành công sửa đổi bài tập")
             msg.setText("Chi tiết câu đã được chỉnh sửa")
             with open(filename) as f:
-                msg.setDetailedText(f'Chi tiết gốc:\n{save_text.details[self.list_assignments.currentRow()]}\n\nChi tiết sau khi sửa đổi:\n{self.assignment_details.toPlainText()}')
+                msg.setDetailedText(
+                    f"Chi tiết gốc:\n{save_text.details[self.list_assignments.currentRow()]}\n\nChi tiết sau khi sửa đổi:\n{self.assignment_details.toPlainText()}"
+                )
             msg.setIcon(QMessageBox.Information)
-            msg.setStandardButtons(QMessageBox.Ok|QMessageBox.Cancel)
+            msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
             msg.buttonClicked.connect(popup_button)
             msg.exec_()
 
         def popup_button(i):
             save_text.changed = False if i.text().lower() == "cancel" else True
-
 
         def open_edit_form():
             window = EditWindow()
@@ -139,15 +157,17 @@ class UIFunctions(MainWindow):
 
     @classmethod
     def student_gui_config(cls, self):
-        self.main_btn.setText('Kiểm tra')
-        self.main_btn.setStyleSheet('''QPushButton {background-color: rgb(224, 150, 0);}
-        QPushButton:hover {background-color: rgba(224, 150, 0, 150);}''')
+        self.main_btn.setText("Kiểm tra")
+        self.main_btn.setStyleSheet(
+            """QPushButton {background-color: rgb(224, 150, 0);}
+        QPushButton:hover {background-color: rgba(224, 150, 0, 150);}"""
+        )
 
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     # role = 'student'
-    role = 'teacher'
+    role = "teacher"
     window = MainWindow(role)
     window.move(GetSystemMetrics(0) - 300, 0)
     window.show()

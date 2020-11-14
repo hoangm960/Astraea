7d6ece8fb53f15230b3943efe83dbf5d5c479a05
nhatminh2901 <hoangm960@gmail.com>
2020-11-14 20:19:26 +0700

    bruh

--- login_main.py
+++ login_main.py
@@ -2,9 +2,8 @@ from PyQt5 import QtCore, QtWidgets
 from PyQt5.QtGui import QColor
 from PyQt5.QtWidgets import QApplication, QMainWindow, QSizeGrip, QWidget
 import sys
-from Login_gui import Ui_MainWindow
-from Loading_Screen import Ui_Loading_Screen
 from PyQt5.QtCore import Qt
+from PyQt5 import uic
 
 GLOBAL_STATE = False
 counter = 0
@@ -12,8 +11,7 @@ counter = 0
 class Loading_Screen(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self, None, QtCore.Qt.WindowStaysOnTopHint)
-        self.ui = Ui_Loading_Screen()
-        self.ui.setupUi(self)
+        uic.loadUi("UI_files/Loading_Screen.ui", self)
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground) 
         self.timer = QtCore.QTimer()
@@ -22,7 +20,7 @@ class Loading_Screen(QMainWindow):
         self.show()
     def progress(self):
         global counter
-        self.ui.progressBar.setValue(counter)
+        self.progressBar.setValue(counter)
         if counter>100:
             self.timer.stop()
             self.main = LoginWindow()
@@ -34,12 +32,11 @@ class Loading_Screen(QMainWindow):
 class LoginWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self, None, QtCore.Qt.WindowStaysOnTopHint)
-        self.ui = Ui_MainWindow()
-        self.ui.setupUi(self)
+        uic.loadUi("UI_Files/Login_gui.ui", self)
         screen = app.primaryScreen()
         size = screen.size()    
-        self.ui.OkCancelFrame.hide()
-        self.ui.Accept.clicked.connect(lambda: self.close())
+        self.OkCancelFrame.hide()
+        self.Accept.clicked.connect(lambda: self.close())
         self.move(round(size.width()/10), round(size.height()/50))
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
         self.setAttribute(QtCore.Qt.WA_TranslucentBackground) 
@@ -53,60 +50,60 @@ class LoginWindow(QMainWindow):
             else:
                 GLOBAL_STATE = False
                 self.showNormal()
-        self.ui.btn_minimize.clicked.connect(lambda: self.showMinimized())
-        self.ui.btn_quit.clicked.connect(lambda: self.ui.OkCancelFrame.show())        
-        self.sizegrip = QSizeGrip(self.ui.frame_grip)
+        self.btn_minimize.clicked.connect(lambda: self.showMinimized())
+        self.btn_quit.clicked.connect(lambda: self.OkCancelFrame.show())        
+        self.sizegrip = QSizeGrip(self.frame_grip)
         self.sizegrip.setStyleSheet("QSizeGrip { background-color: none; width: 20px; height: 20px; margin: 5px; border-radius: 10px; } QSizeGrip:hover { background-color: rgb(66, 0, 99);}")
         self.sizegrip.setToolTip("This was locked")
         #</>
-        self.ui.SignIn_Bt.clicked.connect(lambda: self.check_SI())
-        self.ui.SignUp_Bt.clicked.connect(lambda: self.check_SU())
-        self.ui.Complete_Frame.hide()    
-        self.ui.SignUp_Frame.hide()
+        self.SignIn_Bt.clicked.connect(lambda: self.check_SI())
+        self.SignUp_Bt.clicked.connect(lambda: self.check_SU())
+        self.Complete_Frame.hide()    
+        self.SignUp_Frame.hide()
         self.Default_Check()
     def Default_Check(self): 
-        self.ui.frameError.hide()
-        self.ui.Error_PassRan.hide()
-        self.ui.Error_NameRan.hide() 
-        self.ui.Error_SpecialCr.hide()    
-        self.ui.Error_NamePass.hide()
+        self.frameError.hide()
+        self.Error_PassRan.hide()
+        self.Error_NameRan.hide() 
+        self.Error_SpecialCr.hide()    
+        self.Error_NamePass.hide()
 
     def check_SI(self):
         self.Default_Check()
-        name = self.ui.NameBox_SI.text().lower()
-        password = self.ui.PassBox_SI.text().lower()
+        name = self.NameBox_SI.text().lower()
+        password = self.PassBox_SI.text().lower()
         if name == '' or password == '':
-            self.ui.frameError.show()
-            self.ui.Error_NamePass.show()    
+            self.frameError.show()
+            self.Error_NamePass.show()    
      
     def check_SU(self):
         check = True
         self.Default_Check()
-        name = self.ui.NameBox_SU.text().lower()
-        password = self.ui.PassBox_SU.text().lower()
+        name = self.NameBox_SU.text().lower()
+        password = self.PassBox_SU.text().lower()
         if len(name)<6:
-            self.ui.frameError.show()
-            self.ui.Error_NameRan.show()
+            self.frameError.show()
+            self.Error_NameRan.show()
             check = False
         elif len(password)<3:
-            self.ui.Error_PassRan.show()
-            self.ui.frameError.show()
+            self.Error_PassRan.show()
+            self.frameError.show()
             check = False
         else:
             for word in name:
                 if word not in 'qwertyuiopasdfghjklzxcvbnm1234567890 ':
-                    self.ui.frameError.show()
-                    self.ui.Error_SpecialCr.show()
+                    self.frameError.show()
+                    self.Error_SpecialCr.show()
                     check = False     
                 else:
                     for word in password:
                         if word not in 'qwertyuiopasdfghjklzxcvbnm1234567890':
-                            self.ui.Error_SpecialCr.show()
-                            self.ui.frameError.show()
+                            self.Error_SpecialCr.show()
+                            self.frameError.show()
                             check = False
         if check == True:
-            self.ui.SignUp_Frame.hide()
-            self.ui.Complete_Frame.show()
+            self.SignUp_Frame.hide()
+            self.Complete_Frame.show()
 
 if __name__ == "__main__":    
     app = QApplication(sys.argv)

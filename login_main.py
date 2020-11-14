2fb8a73e30c06dce8f08e668eb880be5e7e7ac87
nhatminh2901 <hoangm960@gmail.com>
2020-11-14 22:09:10 +0700

    linked

--- login_main.py
+++ login_main.py
@@ -4,78 +4,66 @@ from PyQt5.QtWidgets import QApplication, QMainWindow, QSizeGrip, QWidget
 import sys
 from PyQt5.QtCore import Qt
 from PyQt5 import uic
+from win32api import GetSystemMetrics
+import main_ui
 
-GLOBAL_STATE = False
-counter = 0
-#</>-------------------
-class Loading_Screen(QMainWindow):
-    def __init__(self):
-        QMainWindow.__init__(self, None, QtCore.Qt.WindowStaysOnTopHint)
-        uic.loadUi("UI_files/Loading_Screen.ui", self)
-        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground) 
-        self.timer = QtCore.QTimer()
-        self.timer.timeout.connect(self.progress)
-        self.timer.start(20)
-        self.show()
-    def progress(self):
-        global counter
-        self.progressBar.setValue(counter)
-        if counter>100:
-            self.timer.stop()
-            self.main = LoginWindow()
-            self.main.show()
-            self.close()
-        counter += 1
-#</>------------------
 
 class LoginWindow(QMainWindow):
+    GLOBAL_STATE = False
+
     def __init__(self):
         QMainWindow.__init__(self, None, QtCore.Qt.WindowStaysOnTopHint)
         uic.loadUi("UI_Files/Login_gui.ui", self)
-        screen = app.primaryScreen()
-        size = screen.size()    
         self.OkCancelFrame.hide()
         self.Accept.clicked.connect(lambda: self.close())
-        self.move(round(size.width()/10), round(size.height()/50))
+        self.move(round(GetSystemMetrics(0) / 10), round(GetSystemMetrics(1)/ 50))
         self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
-        self.setAttribute(QtCore.Qt.WA_TranslucentBackground) 
-        #<> Set Toolbar Button
-        GLOBAL_STATE = False
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        # <> Set Toolbar Button
+
         def State_change_size():
-            global GLOBAL_STATE
-            if GLOBAL_STATE == False:
-                GLOBAL_STATE = True
+            self.GLOBAL_STATE
+            if self.GLOBAL_STATE == False:
+                self.GLOBAL_STATE = True
                 self.showFullScreen()
             else:
-                GLOBAL_STATE = False
+                self.GLOBAL_STATE = False
                 self.showNormal()
+
         self.btn_minimize.clicked.connect(lambda: self.showMinimized())
-        self.btn_quit.clicked.connect(lambda: self.OkCancelFrame.show())        
+        self.btn_quit.clicked.connect(lambda: self.OkCancelFrame.show())
+
         self.sizegrip = QSizeGrip(self.frame_grip)
-        self.sizegrip.setStyleSheet("QSizeGrip { background-color: none; width: 20px; height: 20px; margin: 5px; border-radius: 10px; } QSizeGrip:hover { background-color: rgb(66, 0, 99);}")
+        self.sizegrip.setStyleSheet(
+            "QSizeGrip { background-color: none; width: 20px; height: 20px; margin: 5px; border-radius: 10px; } QSizeGrip:hover { background-color: rgb(66, 0, 99);}"
+        )
         self.sizegrip.setToolTip("This was locked")
-        #</>
+
         self.SignIn_Bt.clicked.connect(lambda: self.check_SI())
         self.SignUp_Bt.clicked.connect(lambda: self.check_SU())
-        self.Complete_Frame.hide()    
+
+        self.Complete_Frame.hide()
         self.SignUp_Frame.hide()
         self.Default_Check()
-    def Default_Check(self): 
+
+    def Default_Check(self):
         self.frameError.hide()
         self.Error_PassRan.hide()
-        self.Error_NameRan.hide() 
-        self.Error_SpecialCr.hide()    
+        self.Error_NameRan.hide()
+        self.Error_SpecialCr.hide()
         self.Error_NamePass.hide()
 
     def check_SI(self):
         self.Default_Check()
         name = self.NameBox_SI.text().lower()
         password = self.PassBox_SI.text().lower()
-        if name == '' or password == '':
+        if name == "" or password == "":
             self.frameError.show()
-            self.Error_NamePass.show()    
-     
+            self.Error_NamePass.show()
+        else:
+            self.close()
+            main_ui.main()
+
     def check_SU(self):
         check = True
         self.Default_Check()
@@ -91,13 +79,13 @@ class LoginWindow(QMainWindow):
             check = False
         else:
             for word in name:
-                if word not in 'qwertyuiopasdfghjklzxcvbnm1234567890 ':
+                if word not in "qwertyuiopasdfghjklzxcvbnm1234567890 ":
                     self.frameError.show()
                     self.Error_SpecialCr.show()
-                    check = False     
+                    check = False
                 else:
                     for word in password:
-                        if word not in 'qwertyuiopasdfghjklzxcvbnm1234567890 ':
+                        if word not in "qwertyuiopasdfghjklzxcvbnm1234567890 ":
                             self.Error_SpecialCr.show()
                             self.frameError.show()
                             check = False
@@ -105,11 +93,35 @@ class LoginWindow(QMainWindow):
             self.SignUp_Frame.hide()
             self.Complete_Frame.show()
 
-if __name__ == "__main__":    
+
+# </>-------------------
+class Loading_Screen(QMainWindow):
+    counter = 0
+
+    def __init__(self):
+        QMainWindow.__init__(self, None, QtCore.Qt.WindowStaysOnTopHint)
+        uic.loadUi("UI_files/Loading_Screen.ui", self)
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+        self.timer = QtCore.QTimer()
+        self.timer.timeout.connect(self.progress)
+        self.timer.start(20)
+        self.show()
+
+    def progress(self):
+        self.progressBar.setValue(self.counter)
+        if self.counter > 100:
+            self.timer.stop()
+            self.main = LoginWindow()
+            self.main.show()
+            self.close()
+        self.counter += 1
+
+done = False
+def main():
+    done = True
     app = QApplication(sys.argv)
     splash_window = Loading_Screen()
+    splash_window.move(round(GetSystemMetrics(0) / 4), round(GetSystemMetrics(1) / 4))
     splash_window.show()
-    screen = app.primaryScreen()
-    size = screen.size()
-    splash_window.move(round(size.width()/4), round(size.height()/4))
     sys.exit(app.exec_())
---
+++ main_ui.py
@@ -0,0 +1,178 @@
+import os
+import pickle
+import subprocess
+import sys
+
+import win32con
+import win32gui
+from PyQt5 import QtCore
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QColor
+from PyQt5.QtWidgets import (
+    QApplication,
+    QDialogButtonBox,
+    QGraphicsDropShadowEffect,
+    QMainWindow,
+    QMessageBox,
+    QPushButton,
+    QSizeGrip,
+    QWidget,
+)
+from win32api import GetSystemMetrics
+
+from edit_main import EditWindow
+from PyQt5 import uic
+from UI_Files import Resources
+from login_main import *
+
+
+UI_MAIN_PATH = "UI_Files/ui_main.ui"
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, role):
+        QMainWindow.__init__(self, None, QtCore.Qt.WindowStaysOnTopHint)
+        uic.loadUi(UI_MAIN_PATH, self)
+        self.role = role
+        UIFunctions.uiDefinitions(self)
+
+
+class UIFunctions(MainWindow):
+    ASSIGNMENTS_PATH = "data/Lesson/assignments.txt"
+    DETAILS_PATH = "data/Lesson/assignment_details.list"
+
+    @classmethod
+    def uiDefinitions(cls, self):
+        self.setWindowFlag(QtCore.Qt.FramelessWindowHint)
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+
+        self.shadow = QGraphicsDropShadowEffect(self)
+        self.shadow.setBlurRadius(20)
+        self.shadow.setXOffset(0)
+        self.shadow.setYOffset(0)
+        self.shadow.setColor(QColor(0, 0, 0, 100))
+        self.bg_frame.setGraphicsEffect(self.shadow)
+
+        self.btn_minimize.clicked.connect(lambda: self.showMinimized())
+        self.btn_quit.clicked.connect(lambda: cls.close_pg(self))
+
+        # UIFunctions.open_vscode()
+
+        UIFunctions.load_assignments(self, cls.ASSIGNMENTS_PATH)
+        self.list_assignments.itemPressed.connect(
+            lambda: UIFunctions.load_details(self, cls.DETAILS_PATH)
+        )
+
+        UIFunctions.define_role(self)
+
+    @classmethod
+    def open_vscode(cls):
+        file = os.path.expandvars("%LOCALAPPDATA%/Programs/Microsoft VS Code/Code.exe")
+        subprocess.call(file)
+        cls.pg = win32gui.FindWindow(None, "Visual Studio Code")
+        x0, y0, x1, y1 = win32gui.GetWindowRect(cls.pg)
+        w = x1 - x0
+        h = y1 - y0
+        win32gui.MoveWindow(cls.pg, 0, 0, w + 45, h, True)
+
+    @classmethod
+    def close_pg(cls, self):
+        try:
+            win32gui.PostMessage(cls.pg, win32con.WM_CLOSE, 0, 0)
+        except:
+            pass
+
+        self.close()
+
+    @classmethod
+    def load_assignments(cls, self, filename):
+        with open(filename) as f:
+            lines = f.readlines()
+            for line in lines:
+                self.list_assignments.addItem(line.rstrip("\n"))
+
+    @classmethod
+    def load_details(cls, self, filename):
+        with open(filename, "rb") as f:
+            details = pickle.load(f)
+            self.assignment_details.setText(
+                details[self.list_assignments.currentRow()].rstrip("\n")
+            )
+
+    @classmethod
+    def define_role(cls, self):
+        if self.role.lower() == "teacher":
+            cls.teacher_gui_config(self)
+        if self.role.lower() == "student":
+            cls.student_gui_config(self)
+
+    @classmethod
+    def teacher_gui_config(cls, self):
+        self.main_btn.setText("Sửa đổi")
+        self.main_btn.setStyleSheet(
+            """QPushButton {background-color: rgb(59, 143, 14);}
+        QPushButton:hover {background-color: rgba(59, 143, 14, 150);}"""
+        )
+        self.main_btn.clicked.connect(lambda: open_edit_form())
+
+        self.assignment_details.setReadOnly(False)
+        self.confirmButton = QDialogButtonBox(self.frame_content_hint)
+        self.confirmButton.setStandardButtons(QDialogButtonBox.Ok)
+        self.confirmButton.setObjectName("confirmButton")
+        self.verticalLayout_4.addWidget(self.confirmButton)
+        self.confirmButton.accepted.connect(lambda: save_text(cls.DETAILS_PATH))
+
+        def save_text(filename):
+            save_text.changed = True
+            with open(filename, "rb") as f:
+                save_text.details = pickle.load(f)
+            if (
+                save_text.details[self.list_assignments.currentRow()]
+                != self.assignment_details.toPlainText()
+            ):
+                show_confirm_mess(filename)
+            save_text.details[
+                self.list_assignments.currentRow()
+            ] = self.assignment_details.toPlainText()
+            if save_text.changed:
+                with open(filename, "wb") as f:
+                    pickle.dump(save_text.details, f)
+            else:
+                cls.load_details(self, filename)
+
+        def show_confirm_mess(filename):
+            msg = QMessageBox()
+            msg.setWindowTitle("Thành công sửa đổi bài tập")
+            msg.setText("Chi tiết câu đã được chỉnh sửa")
+            with open(filename) as f:
+                msg.setDetailedText(
+                    f"Chi tiết gốc:\n{save_text.details[self.list_assignments.currentRow()]}\n\nChi tiết sau khi sửa đổi:\n{self.assignment_details.toPlainText()}"
+                )
+            msg.setIcon(QMessageBox.Information)
+            msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel)
+            msg.buttonClicked.connect(popup_button)
+            msg.exec_()
+
+        def popup_button(i):
+            save_text.changed = False if i.text().lower() == "cancel" else True
+
+        def open_edit_form():
+            window = EditWindow()
+            window.show()
+
+    @classmethod
+    def student_gui_config(cls, self):
+        self.main_btn.setText("Kiểm tra")
+        self.main_btn.setStyleSheet(
+            """QPushButton {background-color: rgb(224, 150, 0);}
+        QPushButton:hover {background-color: rgba(224, 150, 0, 150);}"""
+        )
+
+def main():
+    # role = 'student'
+    role = "teacher"
+    window = MainWindow(role)
+    window.move(GetSystemMetrics(0) - 300, 0)
+    window.show()
+
+

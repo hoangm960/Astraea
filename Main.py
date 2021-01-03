491cbe1d855e3f537a914223f89b72612ab56cb5
nhatminh2901 <hoangm960@gmail.com>
2021-01-03 20:41:00 +0700

    fix Main auto create opened_doc file

--- Main.py
+++ Main.py
@@ -13,13 +13,14 @@ def create_file():
     ENCRYPTION_PATH = "./data/encryption/"
     OPENED_RESULT_PATH = "./data/results/"
     OPENED_ASSIGNMENT_PATH = "./data/Users/opened_assignment.oa"
-    # OPENED_DOC = "./data/Users/opened_doc.od"
+    OPENED_DOC = "./data/Users/opened_doc.od"
 
 
     Path(USER_PATH).mkdir(parents=True, exist_ok=True)
     Path(ENCRYPTION_PATH).mkdir(parents=True, exist_ok=True)
     Path(OPENED_RESULT_PATH).mkdir(parents=True, exist_ok=True)
-    # Path(OPENED_DOC).mkdir(parents=True, exist_ok=True)
+    if not os.path.exists(OPENED_DOC):
+        open(OPENED_DOC, "w").close()
     if not os.path.exists(OPENED_ASSIGNMENT_PATH):
         open(OPENED_ASSIGNMENT_PATH, "w").close()
 

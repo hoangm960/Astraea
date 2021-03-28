3ee149d6356f61039b6cd27457435fab40dab6a8
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2021-03-28 16:03:30 +0700

    main cleanup

--- Main.py
+++ Main.py
@@ -11,28 +11,34 @@ app = QApplication([])
 screen_resolution = app.desktop().screenGeometry()
 SCREEN_WIDTH, SCREEN_HEIGHT = screen_resolution.width(), screen_resolution.height()*0.953
 
-def create_file():
+def create():
+    def create_file(file):
+        if not os.path.exists(file):
+            open(file, "w").close()
+    def create_dir(dir):
+        Path(dir).mkdir(parents=True, exist_ok=True)
+
     USER_PATH = "./data/Users/"
     ENCRYPTION_PATH = "./data/encryption/"
     OPENED_RESULT_PATH = "./data/results/"
     OPENED_ASSIGNMENT_PATH = "./data/Users/opened_assignment.oa"
     OPENED_DOC = "./data/Users/opened_doc.od"
     OPENED_ROOM_PATH = "./data/Users/opened_room.or"
+    OPENED_DOC_CONTENT = "./data/Users/opened_doc_content.html"
 
-    Path(USER_PATH).mkdir(parents=True, exist_ok=True)
-    Path(ENCRYPTION_PATH).mkdir(parents=True, exist_ok=True)
-    Path(OPENED_RESULT_PATH).mkdir(parents=True, exist_ok=True)
-    if not os.path.exists(OPENED_DOC):
-        open(OPENED_DOC, "w").close()
-    if not os.path.exists(OPENED_ASSIGNMENT_PATH):
-        open(OPENED_ASSIGNMENT_PATH, "w").close()
-    if not os.path.exists(OPENED_ROOM_PATH):
-        open(OPENED_ROOM_PATH, 'w').close()
+
+    paths = [USER_PATH, ENCRYPTION_PATH, OPENED_RESULT_PATH]
+    files = [OPENED_DOC_CONTENT, OPENED_ROOM_PATH, OPENED_DOC, OPENED_ASSIGNMENT_PATH]
+
+    for dir in paths:
+        create_dir(dir)
+    for file in files:
+        create_file(file)
 
 
 if __name__ == "__main__":
 
-    create_file()
+    create()
 
     import login_main
     login_main.main(VERSION)

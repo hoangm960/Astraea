64f48dafc06bd51464d96b6905c2de52e2b46ce2
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2021-11-01 23:00:14 +0700

    clean

--- models/user.py
+++
@@ -1,7 +0,0 @@
-class User:
-    def __init__(self, id, name, name_user, password, role):
-        self.id = id
-        self.name = name
-        self.name_user = name_user
-        self.password = password
-        self.role = role

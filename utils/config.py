ea72d37c89fb42b572c8c5eee1c1b4c292471f39
hoangm960 <63330514+hoangm960@users.noreply.github.com>
2021-11-20 06:33:49 +0700

    fix find_ide

--- utils/config.py
+++ utils/config.py
@@ -10,16 +10,6 @@ def screen_resolution():
     screen_resolution = app.desktop().availableGeometry()
     return screen_resolution.width(), screen_resolution.height()
 
-def install_ide():
-    p = subprocess.call("pip3 install thonny")
-    if p.returncode == 0:
-        subprocess.Popen(["thonny"], shell=True)
-        time.sleep(3)
-        pg = find_ide()
-        pg.activate()
-        auto.press("enter")
-        pg.close()
-
 def find_ide():
     import pygetwindow as gw
 
@@ -34,5 +24,13 @@ def find_ide():
                 break
     if gw.getWindowsWithTitle(ide_title):
         return gw.getWindowsWithTitle(ide_title)[0]
+    
+def install_ide():
+    p = subprocess.run("pip3 install thonny")
+    if p.returncode == 0:
+        subprocess.Popen(["thonny"], shell=True)
+        time.sleep(3)
+        auto.click('utils/click.png')
+        
 
 SCREEN_WIDTH, SCREEN_HEIGHT = screen_resolution()
